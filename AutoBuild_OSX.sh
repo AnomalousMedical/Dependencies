1efae9b767c296b89b461771f958dce3eb72546c
Andrew Piper <andrewpiper@anomalousmedical.com>
2016-07-10 14:05:44 -0400

    Removed mono stuff from OS X that is no longer needed.

--- AutoBuild_OSX.sh
+++ AutoBuild_OSX.sh
@@ -7,13 +7,7 @@ THIS_FOLDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 sh $THIS_FOLDER/OgreDeps/AutoBuild_OSX.sh
 sh $THIS_FOLDER/Ogre/AutoBuild_OSX.sh
 sh $THIS_FOLDER/Bullet/AutoBuild_OSX.sh
-sh $THIS_FOLDER/DotNetZip/AutoBuild_Xbuild.sh
-sh $THIS_FOLDER/FreeImage.NET/AutoBuild_Xbuild.sh
-sh $THIS_FOLDER/Irony/AutoBuild_Xbuild.sh
 sh $THIS_FOLDER/libRocket/AutoBuild_OSX.sh
-sh $THIS_FOLDER/Lucene.net/AutoBuild_Xbuild.sh
-sh $THIS_FOLDER/Mono/AutoBuild_Xbuild.sh
-sh $THIS_FOLDER/Mono/OSXBuild/MakeFramework.sh
 sh $THIS_FOLDER/MSBuild/AutoBuild_Xbuild.sh
 sh $THIS_FOLDER/MyGUI/AutoBuild_OSX.sh
 sh $THIS_FOLDER/oggvorbis/AutoBuild_OSX.sh
