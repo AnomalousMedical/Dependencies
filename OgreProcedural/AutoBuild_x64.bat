c3607eb0798e6b6d48bfe9ae92868918d18bc402
Andrew Piper <andrew@threax.com>
2019-02-23 11:19:19 -0500

    Added windows 64 bit build.

---
+++ OgreProcedural/AutoBuild_x64.bat
@@ -0,0 +1,33 @@
+::Configuration Settings
+set GeneratorName=Visual Studio 15 Win64
+set Platform=x64
+set SrcFolder=src
+set BuildFolder=Win64Build
+set SolutionName=OgreProcedural.sln
+
+::Less likely to need to change these.
+set ThisFolder=%~dp0
+set RootDependencyFolder=%ThisFolder%..\
+set CurrentDirectory=%CD%
+
+set BuildPath="%~dp0%BuildFolder%"
+set SrcPath="%~dp0%SrcFolder%"
+
+::Variables need to be set here
+set Variables=-D OGRE_BUILD=%RootDependencyFolder%Ogre\WindowsBuild ^
+-D OGRE_SOURCE=%RootDependencyFolder%Ogre\src ^
+-D OGRE_SOURCE_DIR=%RootDependencyFolder%Ogre\src ^
+-D OgreProcedural_BUILD_SAMPLES=false ^
+-D OgreProcedural_STATIC=true
+
+::Actual build process
+rmdir /s /q %BuildPath%
+mkdir %BuildPath%
+cd %BuildPath%
+
+cmake -G "%GeneratorName%" %Variables% %SrcPath%
+
+msbuild.exe /m "%SolutionName%" /property:Configuration=Debug;Platform=%Platform%
+msbuild.exe /m "%SolutionName%" /property:Configuration=Release;Platform=%Platform%
+
+cd %CurrentDirectory%
