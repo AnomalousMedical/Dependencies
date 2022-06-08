a725bd7214dd8ed1791806591ccb8b01054c8a8f
Andrew Piper <andrew@threax.com>
2022-06-08 15:24:36 -0400

    Add a file to build just the adventure prereqs.

---
+++ AdventureOnly_x64.bat
@@ -0,0 +1,8 @@
+::Best to do Ogre first
+call OgreDeps\AutoBuild_x64.bat
+
+call Diligent\AutoBuild_x64.bat
+
+::Others in alphabetical order
+call oggvorbis\AutoBuild_x64.bat
+call OpenALSoft\AutoBuild_x64.bat
